c25ebbb82b03bc91b00661c50f9e3c2c030bada2
YuvaraR <YuvaraR@HYD003575.pii.pxl.int>
2019-07-22 17:14:50 +0530

    test

--- src/main/java/com/rajeshyuvaraj/SpringBootHelloWorld/SampleController.java
+++ src/main/java/com/rajeshyuvaraj/SpringBootHelloWorld/SampleController.java
@@ -8,10 +8,14 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
+	
+	
 
+	//testign the code base
 	@RequestMapping("/")
 	@ResponseBody
 	String home() {
+		
 		return "Hello World!";
 	}
 	public static void main(String[] args) throws Exception {
